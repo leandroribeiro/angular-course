d0d229c1d65c2995724e811ce0cf492645e95793
Leandro Ribeiro <github@leandroribeiro.com>
2020-06-11 21:40:51 -0300

    Conteúdo até aula 29

--- typescript-getstarted/section4/lesson16/src/app/app.component.ts
+++ typescript-getstarted/section4/lesson16/src/app/app.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Student} from './student/student.model';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app works!';
+  students: Student[] = [
+    {name: 'Luke', isJedi: true, temple: 'Coruscant'},
+    {name: 'Leia', isJedi: false},
+    {name: 'Han Solo', isJedi: false}
+  ];
 }
