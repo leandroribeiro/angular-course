5477c6b931047dbb01f063c7d2c4c96366916a54
Leandro Ribeiro <>
2020-06-29 21:12:52 -0300

    Conteúdo até aula 59

--- section5/meat-app/src/app/app.routes.ts
+++ section5/meat-app/src/app/app.routes.ts
@@ -5,10 +5,11 @@ import {RestaurantsComponent} from './restaurants/restaurants.component';
 import {RestaurantDetailComponent} from './restaurant-detail/restaurant-detail.component';
 import {MenuComponent} from './restaurant-detail/menu/menu.component';
 import {ReviewsComponent} from './restaurant-detail/reviews/reviews.component';
+import {OrderComponent} from './order/order.component';
 
 export const ROUTES: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'about', component: AboutComponent},
+  
   {path: 'restaurants', component: RestaurantsComponent},
   {
     path: 'restaurants/:id', component: RestaurantDetailComponent,
@@ -18,4 +19,6 @@ export const ROUTES: Routes = [
       {path: 'reviews', component: ReviewsComponent}
     ]
   },
+  {path: 'about', component: AboutComponent},
+  {path: 'order', component: OrderComponent},
 ];
